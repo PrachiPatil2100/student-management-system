baa78d206b2b8284c3e90071c55e9e32148618ca
Prachi Patil <prachiptl2000@gmail.com>
2026-06-29 23:32:00 +0530

    Added persistent data storage using JSON

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-students = []
+import json
+
+def load_data():
+    try:
+        with open("students_data.json","r") as file:
+            return json.load(file)
+
+    except FileNotFoundError:
+        return[]
+    
+
+students = load_data()
+
+def save_data():
+    with open ("students_data.json","w") as file:
+        json.dump(students,file, indent=4)
 
 def add_student():
     name = input("Enter name of the student: ")
     age = int(input("Enter age of the student:"))
     roll_number = int(input("Enter the roll number of the student:"))
+    for student in students:
+        if student["roll_number"]==roll_number:
+            print("\n⚠️ Error: This Roll Number already exists! Try again.")
+            return
+    
     student_data = { "name":name,
                      "age": age,
                      "roll_number":roll_number
                      }
     students.append(student_data)
-    print("Student added successfully!")
+    print("\n✅Student added successfully!")
 
 def view_students():
     print("\n--- Student List ---")
@@ -20,18 +40,18 @@ def search_student():
     search_roll = int(input("Enter Roll Number to search :"))
     for student in students:
         if student["roll_number"]== search_roll:
-            print(f" Found! Name : {student['name']} , Age : {student['age']}")
+            print(f" \n🔍Found! Name : {student['name']} , Age : {student['age']}")
             return
-    print("Student not Found!")
+    print("\n❌Student not Found!")
     
 def delete_student():
     delete_roll = int(input("Enter Roll Number to delete :"))
     for student in students:
         if student['roll_number'] == delete_roll:
             students.remove(student)
-            print("Student deleted successfully!", student)
+            print("\n🗑️Student deleted successfully!", student)
             return
-    print("Student not found!")
+    print("\n❌Student not found!")
     
 while True:     
 
@@ -49,17 +69,19 @@ while True:
 
     if user_choice == 1:
         add_student()
+        save_data()
     elif user_choice == 2:
         view_students()
     elif user_choice == 3:
         search_student()
     elif user_choice == 4:
         delete_student()
+        save_data()
     elif user_choice == 5:
-        print("Exit")
+        print("\nExiting... Have a great day!")
         break
     else:
-        print("Invalid choice! Please select between 1 and 5.")
+        print("\n⚠️Invalid choice! Please select between 1 and 5.")
 
 
 
